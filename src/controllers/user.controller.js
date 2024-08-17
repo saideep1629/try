b995b0d029bbd62a9a7001c8eda24e4d0dae85b1
saideep1629 <saideepmanve3@gmail.com>
2024-08-17 17:12:54 +0530

    subscription related controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -252,7 +252,11 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
 const getCurrentUser = asyncHandler( async (req, res) => {
     return res.status(200)
-    .json(200, req.user, "fetched current user successfully")
+    .json(
+        new ApiResponse(
+            200, 
+            req.user, 
+            "current user fetched successfully"))
 })
 
 const updateUserDetails = asyncHandler(async (req, res) =>{
@@ -352,6 +356,84 @@ const updatecoverImage = asyncHandler(async (req, res) =>{
 
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) =>{
+    const {username} = req.params
+
+    if (!username.trim()) {
+        throw new ApiError(400, "username is missing")
+    }
+
+   const channel = await User.aggregate([
+    {
+        $match: {
+            username: username
+        }
+    },
+    {
+        $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers"
+        }
+    },
+    {
+        $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo"
+        }
+    },
+    {
+        $addFields: {
+            subscribersCount: {
+                $size: "$subscribers"
+            },
+            channelSubscribeToCount: {
+                $size: "$subscribedTo"
+            },
+            isSubscribed: {
+                $cond: {
+                    if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                    then: true,
+                    else: false
+                }
+            }
+        }
+    },
+    {
+        $project: {
+            fullName: 1,
+            username: 1,
+            email: 1,
+            subscribersCount: 1,
+            channelSubscribeToCount: 1,
+            avatar: 1,
+            coverImage: 1,
+            isSubscribed: 1
+        }
+    }
+   ])
+
+   if(!channel?.length){
+    throw new ApiError(404, "channel does not exits")
+   }
+
+   return res.status(200)
+   .json(
+       new ApiResponse(
+        200,
+        channel[0],
+        "User channel fetched successfully"
+
+       )
+   )
+  
+})
+
+
+
 export {
     registerUser,
     loginUser,
@@ -361,5 +443,6 @@ export {
     getCurrentUser,
     updateUserDetails,
     updateAvatar,
-    updatecoverImage
+    updatecoverImage,
+    getUserChannelProfile
 }
